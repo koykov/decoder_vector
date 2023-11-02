4a62e1f2e12b4c2be06908d9cbe54c0a007bdbec
Konstantin Voykov <koykov0@gmail.com>
2023-11-03 00:06:16 +0200

    Implement XML parser test/bench.

--- parser_test.go
+++ parser_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestParser(t *testing.T) {
 	t.Run("json", testStage)
-	// t.Run("xml", testStage)
+	t.Run("xml", testStage)
 	// // t.Run("yaml", testStage) // uncomment after implement yamlvector
 	// t.Run("url", testStage)
 	// t.Run("hal", testStage)
@@ -12,7 +12,7 @@ func TestParser(t *testing.T) {
 
 func BenchmarkParser(b *testing.B) {
 	b.Run("json", benchStage)
-	// b.Run("xml", benchStage)
+	b.Run("xml", benchStage)
 	// b.Run("yaml", benchStage)
 	// b.Run("url", benchStage)
 	// b.Run("hal", benchStage)
