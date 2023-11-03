15e88ff13e1461fce7c014e73534c9698a2126d2
Konstantin Voykov <koykov0@gmail.com>
2023-11-03 22:35:23 +0200

    Implement ULD and HAL test/bench.

--- parser_test.go
+++ parser_test.go
@@ -5,15 +5,15 @@ import "testing"
 func TestParser(t *testing.T) {
 	t.Run("json", testStage)
 	t.Run("xml", testStage)
-	// // t.Run("yaml", testStage) // uncomment after implement yamlvector
-	// t.Run("url", testStage)
-	// t.Run("hal", testStage)
+	// // t.Run("yaml", testStage) // todo uncomment after implement yamlvector
+	t.Run("url", testStage)
+	t.Run("hal", testStage)
 }
 
 func BenchmarkParser(b *testing.B) {
 	b.Run("json", benchStage)
 	b.Run("xml", benchStage)
-	// b.Run("yaml", benchStage)
-	// b.Run("url", benchStage)
-	// b.Run("hal", benchStage)
+	// b.Run("yaml", benchStage)  // todo uncomment after implement yamlvector
+	b.Run("url", benchStage)
+	b.Run("hal", benchStage)
 }
--- readme.md
+++ readme.md
@@ -2,3 +2,22 @@
 
 Provide [vector](https://github.com/koykov/vector) and [vector_inspector](https://github.com/koykov/vector_inspector)
 features to use in [dyntpl](https://github.com/koykov/decoder).
+
+### Usage
+
+```go
+import _ "github.com/koykov/decoder_vector"
+
+const dec = `ctx.data = vector::parseJSON(source) as vector
+dst.Name = data.name`
+
+const source = `{"name":"foobar"}`
+
+func main() {
+	ctx := decoder.AcquireCtx()
+	ctx.SetStatic("source", source)
+	ctx.Set("dst", dstObj, ObjInspector{})
+	decoder.Decode("example", ctx)
+	println(dstObj.Name) // foobar
+}
+```
