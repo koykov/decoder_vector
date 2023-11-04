37b0f84929931ad8111bf5fc2671a7d369827239
Konstantin Voykov <koykov0@gmail.com>
2023-11-04 23:55:56 +0200

    Cover modifiers with test/bench.

---
+++ mod_test.go
@@ -0,0 +1,13 @@
+package decoder_vector
+
+import "testing"
+
+func TestMod(t *testing.T) {
+	t.Run("coalesce", testStage)
+	t.Run("marshal", testStage)
+}
+
+func BenchmarkMod(b *testing.B) {
+	b.Run("coalesce", benchStage)
+	b.Run("marshal", benchStage)
+}
